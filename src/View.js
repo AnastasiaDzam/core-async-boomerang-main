55cd5e44b6bd5fb01f220655271112c2db693ea0
dxpemxnk <petvik5@yandex.ru>
2024-11-01 14:20:28 +0300

    db_wip_v2

--- src/View.js
+++ src/View.js
@@ -1,12 +1,16 @@
 // Сделаем отдельный класс для отображения игры в консоли.
 
 class View {
-  render() {
+
+
+  render(datatrack) {
     const yourTeamName = 'Elbrus';
+
+    
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(''));
+    console.log(datatrack.join(''));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
